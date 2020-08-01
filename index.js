4d8039446088b1cb5de06c0377ed04e9d879fdd7
Jose Luis Reyes <jl@JLs-MacBook-Pro.local>
2020-08-02 01:05:47 +0800

    added request for villager, bugs, and fish. only fetching id#1 for now

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix, token } = require('./config.json');
+const axios = require('axios').default;
+
+const options = {
+    method: 'GET',
+    headers: {'content-type': 'application/json'}
+}
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -19,6 +25,63 @@ client.on('message', message => {
         message.channel.send('Pong.');
     }else if (command === 'shagne'){
         message.channel.send('Is smol');
+    }else if (command === 'villagers'){
+        villagers = () => {
+            return axios.get('https://acnhapi.com/v1/villagers/ant00')
+                .then((response)=>{
+                    let villager = {
+                        name: response.data.name['name-USen'],
+                        personality: response.data.personality,
+                        birthday: response.data.birthday
+                    } 
+
+                    return villager
+                })
+        }
+
+        villagers()
+            .then(data => {
+                message.channel.send(`Villager Name: ${data.name}\nPersonality: ${data.personality}\nBirthday: ${data.birthday}`);
+            })
+        // message.channel.send(`Villager Name: ${name}\nPersonality: ${personality}\nBirthday: $`);
+    }else if (command === 'fish'){
+        fish = () => {
+            return axios.get('https://acnhapi.com/v1/fish/1')
+                .then((response)=>{
+                    let fish = {
+                        name: response.data.name['name-USen'],
+                        price: response.data.price,
+                        phrase: response.data['museum-phrase']
+                    } 
+
+                    return fish
+                })
+        }
+
+        fish()
+            .then(data => {
+                message.channel.send(`Name: ${data.name}\nPrice: ${data.price}\nPhrase: ${data.phrase}`);
+            })
+        // message.channel.send(`Villager Name: ${name}\nPersonality: ${personality}\nBirthday: $`);
+    }else if (command === 'bugs'){
+        bugs = () => {
+            return axios.get('https://acnhapi.com/v1/bugs/1')
+                .then((response)=>{
+                    let bugs = {
+                        name: response.data.name['name-USen'],
+                        price: response.data.price,
+                        phrase: response.data['museum-phrase']
+                    } 
+
+                    return bugs
+                })
+        }
+
+        bugs()
+            .then(data => {
+                message.channel.send(`Name: ${data.name}\nPrice: ${data.price}\nPhrase: ${data.phrase}`);
+            })
+        // message.channel.send(`Villager Name: ${name}\nPersonality: ${personality}\nBirthday: $`);
     }
 });
 
