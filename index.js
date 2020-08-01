c5cafdd8cde44d574823def556995f4b77de244e
Jose Luis Reyes <jl@JLs-MacBook-Pro.local>
2020-08-01 22:03:16 +0800

    added a shagne command

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', message => {
-    if (message.content === '!ping') {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+
+    console.log(args);
+    console.log(command);
+
+    if (command === 'ping') {
         message.channel.send('Pong.');
+    }else if (command === 'shagne'){
+        message.channel.send('Is smol');
     }
 });
 
-client.login(config.token).catch((error) => console.log(error));
+client.login(token).catch((error) => console.log(error));
