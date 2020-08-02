786fb145a082902922ef55ffee0ba61e172c76bd
JL Reyes <joseluisreyes43@gmail.com>
2020-08-02 21:02:18 +0800

    created embedded message for fish command

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix, token } = require('./config.json');
 const axios = require('axios').default;
+const _ = require('lodash');
 
 const options = {
     method: 'GET',
@@ -18,9 +19,6 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).trim().split(' ');
     const command = args.shift().toLowerCase();
 
-    console.log(args);
-    console.log(command);
-    ////////////////////
     request = () => {
         return axios.get(`https://acnhapi.com/v1/${command}/${args}`)
             .then((response)=>{
@@ -41,6 +39,10 @@ client.on('message', message => {
                 return data
             })
     }
+
+    
+    
+
     if (command === 'ping') {
         message.channel.send('Pong.');
     }else if (command === 'shagne'){
@@ -65,7 +67,24 @@ client.on('message', message => {
     }else if (command === 'fish'){
         request()
             .then(data => {
-                message.channel.send(`Name: ${data.name} \n\nPrice: ${data.price} \n\nCJ Price: ${data.cjprice} \n\nTime: ${data.time} \n\nLocation: ${data.location} \n\nShadow: ${data.shadow} \n\nRarity: ${data.rarity} \n\nNorthern Months Availability: ${data.northAvailability} \n\nSouthern Months Availability: ${data.southAvailability} \n\nMuseum Phrase: ${data.phrase} \nhttps://acnhapi.com/v1/images/${command}/${args}`);
+                const fishEmbed = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle(_.capitalize(data.name))
+                    .setDescription(data.phrase)
+                    .setThumbnail(`https://acnhapi.com/v1/icons/${command}/${args}`)
+                    .addFields(
+                        { name: 'Name', value: _.capitalize(data.name) },
+                        { name: 'Price', value: data.price, inline: true  },
+                        { name: 'CJ Price', value: data.cjprice, inline: true  },
+                        { name: 'Time', value: data.time },
+                        { name: 'Northern Months Availability', value: data.northAvailability},
+                        { name: 'Southern Months Availability', value: data.southAvailability},
+                        { name: 'Shadow', value: data.shadow, inline: true  },
+                        { name: 'Rarity', value: data.rarity, inline: true  },
+                    )
+                    .setImage(`https://acnhapi.com/v1/images/${command}/${args}`)
+                    .setFooter('Contact JL if there is a problem 🤓');
+                message.channel.send(fishEmbed);
             })
     }else if (command === 'bugs'){
         request()
